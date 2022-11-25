3346569c734004e0bda0f87cc86315a622a91470
Oren Leavitt <oleavitt@ix.netcom.com>
2022-11-24 22:37:49 -0800

    Initial Commit

---
+++ ThemeParks/ThemeParksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeParksApp.swift
+//  ThemeParks
+//
+//  Created by Oren Leavitt on 11/24/22.
+//
+
+import SwiftUI
+
+@main
+struct ThemeParksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
